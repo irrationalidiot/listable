cf4f9c9e4f0fac415d3f75a7ce33f3afd2f13ec2
Mark Turner <irrationalidiot@gmail.com>
2011-05-16 11:13:00 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'sqlite3'
+gem 'haml-rails'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listable::Application.config.secret_token = '6e547546ae9e638b2e7f03ce55837c0167e8362c840634b7644364ec08a0f6fbbbaa9b4100fa41468f8eea0fb4d4ae4b4bd962787288a9f86c13c0012a4e977b'
